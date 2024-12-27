cb90a4ef807c36599b4b70696f601dc227dd5356
Praful Shinde <praful.22110550@viit.ac.in>
2024-12-27 12:37:09 +0530

    Working for Now

---
+++ ATM_Machine.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+int amount;
+double balance;
+
+cin>>amount;
+cin>>balance;
+double remain;
+
+if(amount%5==0){
+    if(amount < balance && amount + 0.5 <= balance){
+
+        remain = balance - amount -(0.5);
+        cout<<remain<<setprecision(2)<<endl;
+    }
+    else{
+        cout<<"Invalid Entry"<<endl;
+    }  
+
+
+}
+else{
+ cout<<balance<<setprecision(2)<<endl;   
+}
+
+
+
+
+
+}

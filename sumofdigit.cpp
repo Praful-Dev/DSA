cb39f4a1fa8a66fb046a82714e1e5c31a72c328c
Praful Shinde <praful.22110550@viit.ac.in>
2024-12-28 21:08:38 +0530

    Add comments for understanding

--- sumofdigit.cpp
+++ sumofdigit.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
     int t;
-    cin >> t;   // taking number of test cases
-    while (t--) // running the test cases until t is zero so t will run from t then t-- then t-- and so on until it becomes 0
+    cin >> t;                               // taking number of test cases
+    while (t--)                             // running the test cases until t is zero so t will run from t then t-- then t-- and so on until it becomes 0
     {
         int n;
-        cin >> n; // taking the value of n
+        cin >> n;                           // taking the value of n
 
         ostringstream str1;                  // ostringstream converts int to string
         str1 << n;                           // putting our int value --> n in str1
@@ -15,9 +15,9 @@ int main()
         long long int sum = 0;               // taking sum as 0 because we have to add all the digits
         for (int i = 0; i < s.length(); i++) // running the loop from 0 to the length of the string
         {
-            sum += s[i] - 48; // sum = sum + s[i] - 48  | 48 is the ascii value of 0 so we are subtracting it
-                              // let say for example if s[i] = '4' then s[i] - 48 ---> 52 - 48 = 4 <-- is now integer | 4 ==> 52 achi value
-                              // 4 is stored in sum then suming it with next digit
+            sum += s[i] - 48;                // sum = sum + s[i] - 48  | 48 is the ascii value of 0 so we are subtracting it
+                                             // let say for example if s[i] = '4' then s[i] - 48 ---> 52 - 48 = 4 <-- is now integer | 4 ==> 52 achi value
+                                             // 4 is stored in sum then suming it with next digit
         }
         cout << sum << endl;
     }

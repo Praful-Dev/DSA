21fcaf24d25a7850ecac776fe07a4cabe33d9b1b
Praful Shinde <praful.22110550@viit.ac.in>
2024-12-28 19:10:00 +0530

    Delete old Files

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-void displaychai(){
-//     cout<<"Tea Name: "<<teaName<<endl;
-//     cout<<"Tea Serving: "<<servings<<endl;
-//     cout<<"Ingredients: "<<endl;
-//    for(auto i : ingredients){
-//     cout<<" "<<i<<endl;
-//    }
-// }

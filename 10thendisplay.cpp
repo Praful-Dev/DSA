1fc130ed508a90b99af8cb5fb452fd30ca266cf7
Praful Shinde <praful.22110550@viit.ac.in>
2024-12-30 22:57:44 +0530

    https://www.codechef.com/problems/RECTANGL

---
+++ 10thendisplay.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int t;
+     cin>>t;
+     while(t--){
+         int n;
+         cin >>n;
+         if(n < 10 || n < 0 ){
+             cout<<"Thanks for helping Chef!"<<endl;
+         }
+         else{
+             cout<<"-1"<<endl;
+            
+         }
+         
+         
+         
+         
+         
+     }
+	return 0;
+}
---
+++ LUCKFOUR.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+int t; 
+cin>>t;
+int count=0;
+for(int i=0; i<t; i++){
+    int n;
+    cin>>n;
+    if(n%4==0){
+        count++;
+    }
+
+}
+
+cout<<count<<endl;
+
+
+
+
+    return 0;
+}
